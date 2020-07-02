e708ac78d78a75a66fbc6b3b4db3e94ca2897e4f
Myosis <myosis@Mac-mini-Myosis.local>
2020-07-02 16:30:13 +0300

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './containers/home/index'
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <>
+    <Home />
+          </>
+  );
+};
+
+export default App;
---
+++ src/containers/home/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  ImageBackground
+} from 'react-native';
+import styles from './styles'
+
+const testText = `Welcome
+to Sofaster!`;
+
+const Home = () => {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ImageBackground source={require('../../assets/images/sofaster_welcome_screen.png')} style={styles.image}>
+          <View style={styles.textBox}>
+            <Text style={styles.text}>{testText}</Text>
+          </View> 
+        </ImageBackground>
+      </SafeAreaView>
+    );
+  };
+
+  
+
+export default Home;
